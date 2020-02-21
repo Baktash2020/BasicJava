dc47e44a633ad1e316b657e786db5649ad57c8b1
Nillab Waez <you@example.com>
2020-02-21 13:59:02 -0500

    Updated on 2-21-2020

---
+++ src/BasicTests/ForGithub.java
@@ -0,0 +1,27 @@
+package BasicTests;
+
+public class ForGithub {
+
+	public static void main(String[] args) {
+
+
+		int x= 44;
+		int y=155;
+		
+		if(x==y){
+			System.out.println("x is equal to y");
+		}
+		
+		if (x>y) {
+			
+			System.out.println("x is greater than y");
+		}
+		System.out.println("out of the conidition");
+		
+		
+		
+		
+
+	}
+
+}
